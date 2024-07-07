e1fd53c4ae2bf8c16e4a6e97c50a8e99b06b49a2
aroltakam <arol.takam-tiomo@stud.thga.de>
2024-07-07 14:40:01 +0200

    add include-guard

---
+++ gaussian.h
@@ -0,0 +1,15 @@
+#ifndef GAUSSIAN_H
+#define GAUSSIAN_H
+
+#include <stdio.h>
+
+void print_gaussian_dist(int expected_value, int std_deviation, int amount_of_values, FILE *output_stream);
+
+#endif  // GAUSSIAN_H#ifndef GAUSSIAN_H
+	// #define GAUSSIAN_H
+	//
+	// #include <stdio.h>
+	//
+	// void print_gaussian_dist(int expected_value, int std_deviation, int amount_of_values, FILE *output_stream);
+	//
+	// #endif  // GAUSSIAN_H
